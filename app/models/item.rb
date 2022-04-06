e2d5c02b6b91c82f32c0e96e3f4ffba9cd4d890a
pro-soichiro <dr.1987626@gmail.com>
2022-04-06 03:13:49 +0000

    6.2.2 コールバックの実装と呼び出されるタイミング

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,33 @@
 class Item < ApplicationRecord
+  before_validation :before_valid_message
+
+  validates :name, length: { minimum: 3 }
+
+  after_validation :after_valid_message
+
+  after_save :after_save_message
+
+  around_update :around_update_message
+
+
+  private
+
+  def before_valid_message
+    puts "バリデーション前にコールバックするメソッドです。"
+  end
+
+  def after_valid_message
+    puts "バリデーション後にコールバックするメソッドです。"
+  end
+
+  def after_save_message
+    puts "save後にコールバックするメソッドです。"
+  end
+
+  def around_update_message
+    puts "update開始"
+    yield
+    puts "update終了"
+  end
+
 end

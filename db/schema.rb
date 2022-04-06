9a4c61bacc31cf53bf322662152adbeac3d5a631
pro-soichiro <dr.1987626@gmail.com>
2022-04-06 02:54:00 +0000

    itmeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_013641) do
+ActiveRecord::Schema.define(version: 2022_04_06_025258) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

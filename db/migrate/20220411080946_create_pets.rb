0325daa0774d47e43153a39578c4063a68e70837
pro-soichiro <dr.1987626@gmail.com>
2022-04-11 08:17:56 +0000

    単一テーブル継承(STI)

---
+++ db/migrate/20220411080946_create_pets.rb
@@ -0,0 +1,10 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :type
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_025258) do
+ActiveRecord::Schema.define(version: 2022_04_11_080946) do
 
   create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "type"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

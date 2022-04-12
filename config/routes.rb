ff3d783d29ad56c14c8988a8a8c74fb28af294fb
pro-soichiro <dr.1987626@gmail.com>
2022-04-12 23:25:35 +0000

    resourcesによるルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # index以外の7つのアクションルート
+  # resource :users
+  # 7つのアクション
   resource :users
-  
-  
+
   get 'greetings/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

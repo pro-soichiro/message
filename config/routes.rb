d0d0e89ba15962b8f3e98b88d80037fce77ab739
pro-soichiro <dr.1987626@gmail.com>
2022-04-12 23:36:45 +0000

    pathオプションによるURIの変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # resources :users,as: :customers
   # 対応するコントローラー名を変更する
   # 外部URIに表示される名前と内部で実際に使用するコントローラー名を異なるものとして見せることができる
-  resources :users,controller: :customers
+  # resources :users,controller: :customers
+  # URI側の表示を変えることができる。コントローラー名はそのままなのでcontrollerオプションの逆の作用
+  resources :users,path: :customers
 
   get 'greetings/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
